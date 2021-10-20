cd5d4a06ed2aaae5cad77c7806880a02da73c55a
Denley Hsiao <denley@justtodo.com>
2021-10-20 15:40:59 +0800

    Remove daemon

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@ int main(int argc, const char* argv[])
        .setLogLevel(trantor::Logger::kWarn)
        .addListener("0.0.0.0", 80)
        .setThreadNum(16)
-       .enableRunAsDaemon()
        .run();
 
   return 0;
