0c895bc585daf1c29a369123d0f93a7d795e5a24
Denley Hsiao <denley@justtodo.com>
2021-10-20 15:38:36 +0800

    Add drogon

---
+++ NOTICE.md
@@ -0,0 +1,6 @@
+* drogon是以submodule添加的，clone时注意加上：`--recursive`
+* drogon还包含子模块trantor，注意使用：
+  ```bash
+  cd vendor/drogon
+  git submodule update --init
+  ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
+#include <drogon/drogon.h>
+using namespace drogon;
 
 int main(int argc, const char* argv[])
 {
-  std::cout<<"Hello, World!"<<std::endl;
+  app().setLogPath("./")
+       .setLogLevel(trantor::Logger::kWarn)
+       .addListener("0.0.0.0", 80)
+       .setThreadNum(16)
+       .enableRunAsDaemon()
+       .run();
+
   return 0;
 }
