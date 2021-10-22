ad267cce98c61a5fb39d0341d4b5c8ab2dab6f70
Denley Hsiao <denley@justtodo.com>
2021-10-22 09:03:42 +0800

    Support bootstrap

--- src/controllers/HomeController.cpp
+++ src/controllers/HomeController.cpp
@@ -4,6 +4,9 @@ using namespace drogon;
 void HomeController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
 {
   HttpViewData data;
+  data["title"] ="管理平台";
+  data["company_name"] = "深圳雅果";
+  data["copyright_year"] = "2021";
   data["name"] = req->getParameter("name");
   auto resp = HttpResponse::newHttpViewResponse("HomeView.csp", data);
   callback(resp);
